c32b1c2653d94e80ff92762154e5abdeb722c46b
Sam <sirajudheenam@gmail.com>
2014-03-03 16:06:00 +0530

    First Commit

---
+++ script1.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Welcome to my script"
+echo "This is from echo"
+uname -a
+echo "This is output of 'uname -a' command" 
+time
+echo "This is output of time command"
+date
+echo "This is output of date command"
+which ssh
+echo "This is outout of which ssh command"
